4eb78765cd2e2d8f96825eaba5acbbe404294669
Destiny Boone <destaj.db@gmail.com>
2022-11-01 16:52:37 -0400

    adding ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,28 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [ // after the colon just tells typescript what type of variable this is
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.pexels.com/photos/9203735/pexels-photo-9203735.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
-    new Recipe('Another Test Recipe', 'This is another test', 'https://images.pexels.com/photos/3297367/pexels-photo-3297367.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+    new Recipe('Loaded Baked Potatoes', 
+    'Oven baked potatoes', 
+    'https://images.pexels.com/photos/9203735/pexels-photo-9203735.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    [
+      new Ingredient('potato', 10), 
+      new Ingredient('sour cream', 11), 
+      new Ingredient('cheese', 2),
+      new Ingredient('chives', 1)
+    ]),
+    new Recipe('Salmon Poke Bowl', 
+    'A refreshing fish and rice dish', 
+    'https://images.pexels.com/photos/4828093/pexels-photo-4828093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    [
+      new Ingredient('salmon', 2),
+      new Ingredient('rice', 1),
+      new Ingredient('cucumber', 1)
+    ]),
   ];
 
   getRecipes(){
