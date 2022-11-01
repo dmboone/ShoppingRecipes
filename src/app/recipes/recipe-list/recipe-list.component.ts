5f63c5e988f2604166e055545c1111c1786cfddf
Destiny Boone <destaj.db@gmail.com>
2022-11-01 14:37:22 -0400

    managing recipes in a recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,14 +9,12 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [ // after the colon just tells typescript what type of variable this is
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.pexels.com/photos/9203735/pexels-photo-9203735.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
-    new Recipe('Another Test Recipe', 'This is another test', 'https://images.pexels.com/photos/3297367/pexels-photo-3297367.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
-  ]; 
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe){
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,13 @@
+import { Recipe } from "./recipe.model";
+
 export class RecipeService{
-    
+    private recipes: Recipe[] = [ // after the colon just tells typescript what type of variable this is
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://images.pexels.com/photos/9203735/pexels-photo-9203735.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+    new Recipe('Another Test Recipe', 'This is another test', 'https://images.pexels.com/photos/3297367/pexels-photo-3297367.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'),
+  ];
+
+  getRecipes(){
+    return this.recipes.slice(); // the slice will return an exact copy rather than a direct reference!
 }
+}
+
