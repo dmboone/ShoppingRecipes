caf7fe6278e219d9a8b43bf5740b8e2ef3cdb034
Destiny Boone <destaj.db@gmail.com>
2022-11-04 16:47:39 -0400

    adding navigation to the app

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +7,9 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
-  @Output() featureSelected = new EventEmitter<string>(); // event info can now be accessed by parent
   
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  onSelect(feature: string){
-    this.featureSelected.emit(feature); //emit this event info to parent (by using $event in app.component.html)
   }
 }
