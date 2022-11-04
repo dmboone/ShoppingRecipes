18c3f2fa19f05680d8dfb65e1d6f595df0c84e3e
Destiny Boone <destaj.db@gmail.com>
2022-11-04 16:44:55 -0400

    setting up routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+
+const appRoutes: Routes = [
+    {path: '', redirectTo: '/recipes', pathMatch: 'full'}, // pathMatch property now only redirects if the full path is empty
+    {path: 'recipes', component: RecipesComponent},
+    {path: 'shopping-list', component: ShoppingListComponent},
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{
+
+}
