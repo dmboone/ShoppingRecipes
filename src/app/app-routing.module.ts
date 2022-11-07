70ea5db423390762ffd3ea74896eeaa1bf8b981c
Destiny Boone <destaj.db@gmail.com>
2022-11-07 10:00:03 -0500

    adding editing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
@@ -9,7 +10,9 @@ const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'}, // pathMatch property now only redirects if the full path is empty
     {path: 'recipes', component: RecipesComponent, children: [ // these children will all have /recipes in front of it
         {path: '', component: RecipeStartComponent},
-        {path: ':id', component: RecipeDetailComponent}
+        {path: 'new', component: RecipeEditComponent}, // new should come before dynamic id path! otherwise angular will try to parse the word 'new' as an id rather than simply 'new' which is a predefined path
+        {path: ':id', component: RecipeDetailComponent},
+        {path: ':id/edit', component: RecipeEditComponent}
     ]},
     {path: 'shopping-list', component: ShoppingListComponent},
 ];
