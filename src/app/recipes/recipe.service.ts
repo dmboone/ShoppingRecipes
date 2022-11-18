b1266bc50a940eb1e7caf1d7c7f553a7dbd7dfcf
Destiny Boone <destaj.db@gmail.com>
2022-11-18 10:55:04 -0500

    transforming response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,25 +7,27 @@ import { Recipe } from "./recipe.model";
 @Injectable()
 export class RecipeService{
     recipesChanged = new Subject<Recipe[]>(); // create recipesChanged Subject so we can observe if there has been a change in the list of recipes
-    private recipes: Recipe[] = [ // after the colon just tells typescript what type of variable this is
-    new Recipe('Loaded Baked Potatoes', 
-    'Oven baked potatoes', 
-    'https://images.pexels.com/photos/9203735/pexels-photo-9203735.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    [
-      new Ingredient('potato', 10), 
-      new Ingredient('sour cream', 11), 
-      new Ingredient('cheese', 2),
-      new Ingredient('chives', 1)
-    ]),
-    new Recipe('Salmon Poke Bowl', 
-    'A refreshing fish and rice dish', 
-    'https://images.pexels.com/photos/4828093/pexels-photo-4828093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    [
-      new Ingredient('salmon', 2),
-      new Ingredient('rice', 1),
-      new Ingredient('cucumber', 1)
-    ]),
-  ];
+    // Dummy data
+    // private recipes: Recipe[] = [ // after the colon just tells typescript what type of variable this is
+    // new Recipe('Loaded Baked Potatoes', 
+    // 'Oven baked potatoes', 
+    // 'https://images.pexels.com/photos/9203735/pexels-photo-9203735.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    // [
+    //   new Ingredient('potato', 10), 
+    //   new Ingredient('sour cream', 11), 
+    //   new Ingredient('cheese', 2),
+    //   new Ingredient('chives', 1)
+    // ]),
+    // new Recipe('Salmon Poke Bowl', 
+    // 'A refreshing fish and rice dish', 
+    // 'https://images.pexels.com/photos/4828093/pexels-photo-4828093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    // [
+    //   new Ingredient('salmon', 2),
+    //   new Ingredient('rice', 1),
+    //   new Ingredient('cucumber', 1)
+    // ]),
+    // ];
+    private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService){}
 
