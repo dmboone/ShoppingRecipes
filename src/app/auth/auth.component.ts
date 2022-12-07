38b8c6c1c14104929cda94f6864b3193c04d9a44
Destiny Boone <destaj.db@gmail.com>
2022-12-07 13:14:13 -0500

    preparing programmatic creation

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import {Component} from '@angular/core';
+import {Component, ComponentFactoryResolver} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AlertComponent } from '../shared/alert/alert.component';
 import { AuthResponseData, AuthService } from './auth.service';
 
 @Component({
@@ -14,7 +15,7 @@ export class AuthComponent{
     isLoading = false;
     error: string = null;
 
-    constructor(private authService: AuthService, private router: Router){}
+    constructor(private authService: AuthService, private router: Router, private componentFactoryResolver: ComponentFactoryResolver){}
 
     onSwitchMode(){
         this.isLoginMode = !this.isLoginMode; // just switches the isLoginMode value
@@ -48,6 +49,7 @@ export class AuthComponent{
             {
                 console.log(errorMessage);
                 this.error = errorMessage;
+                this.showErrorAlert(errorMessage);
                 this.isLoading = false;
             }
         );
@@ -57,5 +59,10 @@ export class AuthComponent{
 
     onHandleError(){
         this.error = null;
+    }
+
+    private showErrorAlert(message: string){ // gets called and will add error component programmatically
+        const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+        
     }
 }
