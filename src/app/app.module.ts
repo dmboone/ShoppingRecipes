d6a6bd4966a993414012244701268141063e4f41
Destiny Boone <destaj.db@gmail.com>
2022-12-12 11:42:05 -0500

    understanding shared modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,27 +5,20 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
-import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
     AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -35,6 +28,7 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
+    SharedModule
   ],
   providers: [ // will be able to use these services application wide and make sure there is only one instance
     ShoppingListService, // so any changes should permeate throughout entire application
@@ -47,6 +41,5 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
   ],
                                                   
   bootstrap: [AppComponent],
-  entryComponents: [AlertComponent] // not needed for Angular 9 +
 })
 export class AppModule { }
